4dfcc356f73a0e2d28863b3d9c44f2c39941a4d5
Joshua Hatcher <joshua.abraham.hatcher@gmail.com>
2017-08-18 15:04:57 -0400

    Add customizable temp api keys file

---
+++ api-keys.temp.js
@@ -0,0 +1,7 @@
+// Customize this file with your own Client ID and Client Secret from the Strava developer site.
+// Don't forget to rename this file to api-keys.js before trying to run the app.
+
+module.exports = {
+  id: '<clientId>',
+  secret: '<clientSecret>'
+};
